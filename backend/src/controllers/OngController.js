3b158f8b99fd7baa527aa8445d3ccfcac425590b
Jacksson Andrey <jaab.andrey@gmail.com>
2020-05-30 21:14:32 -0300

    Cração do backend

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,25 @@
+const crypto = require('crypto');
+const conncetion = require('../database/connection');
+
+module.exports = {
+  async index(req, res)  {
+    const ongs = await conncetion('ongs').select('*');
+    return res.json(ongs);
+  },
+
+  async create(req, res) {
+    const { name, email, whatsapp, city, uf } = req.body;
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await conncetion('ongs').insert({
+      id,
+      name, 
+      email,
+      whatsapp,
+      city,
+      uf,
+    });
+    
+    return res.json({ id });
+  }
+};
