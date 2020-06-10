24f452180df81632b5cb92d9e1d533f24b0f89b9
Jacksson Andrey <jaab.andrey@gmail.com>
2020-06-10 13:33:29 -0300

    Inclusão de middlewares e testes de integração

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,17 +4,19 @@ const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
 
+const validations = require('./middlewares/validations');
+
 const routes = express.Router();
 
-routes.post('/sessions', SessionController.create);
+routes.post('/sessions', validations.loginRequired, SessionController.create);
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', validations.createOngs, OngController.create);
 
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', validations.isAuthorization, ProfileController.index);
 
-routes.post('/incidents', IncidentController.create);
-routes.get('/incidents', IncidentController.index);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.post('/incidents', validations.isAuthorization, validations.createIncidents, IncidentController.create);
+routes.get('/incidents', validations.isNumberPaginate, validations.isAuthorization, IncidentController.index);
+routes.delete('/incidents/:id', validations.isNumberDel, IncidentController.delete);
 
 module.exports = routes;
